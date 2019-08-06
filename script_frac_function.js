8bbebbfd4049bdafefcd7362e003031dc57ca005
Justin Canose <50759336+jcanose81@users.noreply.github.com>
2019-08-06 11:17:11 -0400

    Add files via upload

---
+++ script_frac_function.js
@@ -0,0 +1,12 @@
+function findLargeFraction(a,b) {
+
+        console.log("Function running...")
+    a>b ? console.log("a: ", a) : console.log("b: ", b);
+}
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+
+
+findLargeFraction(firstFraction, secondFraction);
+findLargeFraction(7/16,13/25);
